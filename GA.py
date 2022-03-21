af99a974ce19b9435f442f8067e9b83110dd4af5
Atha Hibatullah <athalayana@mail.ugm.ac.id>
2022-03-21 13:38:01 +0700

    Added seleksi turnamen, mutasi, generate more than 1 individu

--- GA.py
+++ GA.py
@@ -40,4 +40,53 @@ def inisialisasi_individu(banyak_birama, anggota_birama, range_nada, komposisi):
         #         quota -= 1
         alt_nums_core.append(alt_nums)
         komposisi[i] = np.array(randnums)
+
     return komposisi, np.array(alt_nums_core)
+
+def seleksi_turnamen(fitness_individu_list, peserta_turnamen, jumlah_turnamen):
+    hasil_seleksi_list = []
+    for i in range(jumlah_turnamen):
+        current_turnamen = []
+        current_turnamen = random.sample(fitness_individu_list, peserta_turnamen)
+        hasil_seleksi_list.append(max(current_turnamen))
+    return hasil_seleksi_list
+
+def mutasi(komposisi, anggota_birama, probabilitas_mutasi, range_nada):
+    operasi = ["+","-"]
+    mengubah_nada = [1,2,3,4,5,6]
+    for i in range(len(komposisi)):
+        stop_mutasi = 0
+        while stop_mutasi < int(anggota_birama*probabilitas_mutasi):
+            kena_mutasi = random.randint(0,anggota_birama-1)
+            if komposisi[i][kena_mutasi] != range_nada:
+                pilih_operasi = random.choice(operasi)
+                if pilih_operasi == "+" and komposisi[i][kena_mutasi] + 7 < range_nada:
+                    komposisi[i][kena_mutasi] += 7
+                    stop_mutasi += 1
+                elif komposisi[i][kena_mutasi]-7 > 0:
+                    komposisi[i][kena_mutasi] -= 7
+                    stop_mutasi += 1
+    
+        stop_mutasi = 0
+        while stop_mutasi < int(anggota_birama*probabilitas_mutasi):
+            kena_mutasi = random.randint(0,anggota_birama-1)
+            if komposisi[i][kena_mutasi] != range_nada:
+                pilih_operasi = random.choice(operasi)
+                pilih_mengubah_nada = random.choice(mengubah_nada)
+                if pilih_operasi == "+" and komposisi[i][kena_mutasi] + pilih_mengubah_nada < range_nada:
+                    komposisi[i][kena_mutasi] += pilih_mengubah_nada
+                    stop_mutasi += 1
+                elif komposisi[i][kena_mutasi]-pilih_mengubah_nada > 0:
+                    komposisi[i][kena_mutasi] -= pilih_mengubah_nada
+                    stop_mutasi += 1
+
+        stop_mutasi = 0
+        while stop_mutasi < int(anggota_birama*probabilitas_mutasi):
+            kena_mutasi = random.randint(0,anggota_birama-1)
+            kena_mutasi_sec = random.randint(0,anggota_birama-1)
+            if komposisi[i][kena_mutasi] != range_nada and komposisi[i][kena_mutasi_sec] != range_nada:
+                komposisi[i][kena_mutasi], komposisi[i][kena_mutasi_sec] = komposisi[i][kena_mutasi_sec], komposisi[i][kena_mutasi]
+                stop_mutasi += 1
+
+
+    return komposisi
--- main.py
+++ main.py
@@ -4,47 +4,69 @@ import GA
 import parameter_fitness as pf
 import fungsi_fitness as ff
 
-banyak_birama = int(input())
+banyak_birama = int(input("Banyak birama: "))
+banyak_individu = int(input("Banyak individu: "))
+probabilitas_mutasi = int(input("Probabilitas mutasi: "))/100
 range_nada = 14+1
 anggota_birama = 10
 komposisi = np.empty((banyak_birama,anggota_birama))
 
-populasi_awal = GA.inisialisasi_individu(banyak_birama, anggota_birama, range_nada, komposisi)
+def generate_populasi(banyak_birama, anggota_birama, range_nada, komposisi, banyak_individu):
+    individu_list = []
+    alt_individu_list = []
+    for i in range(banyak_individu):
+        populasi_awal = GA.inisialisasi_individu(banyak_birama, anggota_birama, range_nada, komposisi)
+        alt_populasi = populasi_awal[1]
+        alt_individu_list.append(alt_populasi)
+        populasi_awal = populasi_awal[0]
+        individu_list.append(populasi_awal)
+        komposisi = np.empty((banyak_birama,anggota_birama))
+    return individu_list, alt_individu_list
+
+# populasi_awal = GA.inisialisasi_individu(banyak_birama, anggota_birama, range_nada, komposisi)
+populasi_awal = generate_populasi(banyak_birama, anggota_birama, range_nada, komposisi, banyak_individu)
 alt_populasi = populasi_awal[1]
 populasi_awal = populasi_awal[0]
+print(populasi_awal)  
 
-print(populasi_awal)
+# def hitung_fitness(populasi_awal, alt_populasi, range_nada, banyak_birama, anggota_birama):
+#     for j in range():
+#         variasi_nada_list = pf.variasi_nada(populasi_awal, range_nada, banyak_birama)
+#         interval_disonan_list = pf.interval_disonan(populasi_awal,anggota_birama,range_nada)
+#         arah_kontur_list = pf.arah_kontur(populasi_awal,anggota_birama)
+#         stabilitas_kontur_list = pf.stabilitas_kontur(alt_populasi, anggota_birama)
+#         variasi_irama_list = pf.variasi_irama(populasi_awal,anggota_birama,range_nada)
+#         rentang_irama_list = pf.rentang_irama(populasi_awal,anggota_birama,range_nada)
+#         fitness_birama_all = []
 
-variasi_nada_list = pf.variasi_nada(populasi_awal, range_nada, banyak_birama)
-interval_disonan_list = pf.interval_disonan(populasi_awal,anggota_birama,range_nada)
-arah_kontur_list = pf.arah_kontur(populasi_awal,anggota_birama)
-stabilitas_kontur_list = pf.stabilitas_kontur(alt_populasi, anggota_birama)
-variasi_irama_list = pf.variasi_irama(populasi_awal,anggota_birama,range_nada)
-rentang_irama_list = pf.rentang_irama(populasi_awal,anggota_birama,range_nada)
+#         for j in range(banyak_birama):
+#             fitness_birama = [variasi_nada_list[j],interval_disonan_list[j],arah_kontur_list[i],stabilitas_kontur_list[i],variasi_irama_list[i],rentang_irama_list[i]]
+#             fitness_birama_all.append(fitness_birama)
 
-fitness_birama_all = []
+# # print(fitness_birama_all)
+# # print(pf.variasi_nada(populasi_awal, range_nada, banyak_birama))
+# # print(pf.interval_disonan(populasi_awal,anggota_birama,range_nada))
+# # print(pf.arah_kontur(populasi_awal,anggota_birama))
+# # print(pf.variasi_irama(populasi_awal,anggota_birama,range_nada))
+# # print(pf.rentang_irama(populasi_awal,anggota_birama,range_nada))
+# sum_fitness_birama = ff.sum_fitness_birama(fitness_birama_all, banyak_birama)
+# sum_fitness_birama_all = ff.sum_fitness_birama_all(sum_fitness_birama)
 
-for i in range(banyak_birama):
-    fitness_birama = [variasi_nada_list[i],interval_disonan_list[i],arah_kontur_list[i],stabilitas_kontur_list[i],variasi_irama_list[i],rentang_irama_list[i]]
-    fitness_birama_all.append(fitness_birama)
+# # print(sum_fitness_birama,sum_fitness_birama_all)
+# interval_sum_list = ff.interval_sum(populasi_awal, anggota_birama)
+# mean_interval_birama = ff.mean_interval_birama(interval_sum_list, anggota_birama)
+# varian_interval_birama = ff.varian_interval_birama(interval_sum_list, mean_interval_birama, anggota_birama)
+# sum_mean_interval_birama = ff.sum_mean_interval_birama(mean_interval_birama)
+# sum_varian_interval_birama = ff.sum_varian_interval_birama(varian_interval_birama)
 
-# print(fitness_birama_all)
-# print(pf.variasi_nada(populasi_awal, range_nada, banyak_birama))
-# print(pf.interval_disonan(populasi_awal,anggota_birama,range_nada))
-# print(pf.arah_kontur(populasi_awal,anggota_birama))
-# print(pf.variasi_irama(populasi_awal,anggota_birama,range_nada))
-# print(pf.rentang_irama(populasi_awal,anggota_birama,range_nada))
-sum_fitness_birama = ff.sum_fitness_birama(fitness_birama_all, banyak_birama)
-sum_fitness_birama_all = ff.sum_fitness_birama_all(sum_fitness_birama)
+# # print(sum_mean_interval_birama,sum_varian_interval_birama)
 
-# print(sum_fitness_birama,sum_fitness_birama_all)
-interval_sum_list = ff.interval_sum(populasi_awal, anggota_birama)
-mean_interval_birama = ff.mean_interval_birama(interval_sum_list, anggota_birama)
-varian_interval_birama = ff.varian_interval_birama(interval_sum_list, mean_interval_birama, anggota_birama)
-sum_mean_interval_birama = ff.sum_mean_interval_birama(mean_interval_birama)
-sum_varian_interval_birama = ff.sum_varian_interval_birama(varian_interval_birama)
+# total_fitness = ff.total_fitness(sum_mean_interval_birama,sum_varian_interval_birama,sum_fitness_birama_all)
+# # print("Total fitness: " ,total_fitness)
 
-# print(sum_mean_interval_birama,sum_varian_interval_birama)
+# # seleksi_list = [1,12,5,13,6,7,98,10,2]
+# # hasil_seleksi = GA.seleksi_turnamen(seleksi_list, 3, len(seleksi_list))
+# # print(seleksi_list)
+# # print(hasil_seleksi)
 
-total_fitness = ff.total_fitness(sum_mean_interval_birama,sum_varian_interval_birama,sum_fitness_birama_all)
-print(total_fitness)
+# # print(GA.mutasi(populasi_awal, anggota_birama, probabilitas_mutasi, range_nada))
